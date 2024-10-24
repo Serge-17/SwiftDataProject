0a533c94b82fdee43a9d0dfd6edd300e950921ba
Sergey Eliseev <sergei.eliseev17@icloud.com>
2024-10-24 12:30:11 +0300

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Serge Eliseev on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
